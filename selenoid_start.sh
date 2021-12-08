6eae3e795ab670b09b239dfd6b9d79f56895c04e
Denis <forest152324@gmail.com>
2021-12-08 22:25:54 +0400

    added sudo

--- selenoid_start.sh
+++ selenoid_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "==============================> Pulling Chrome"
-docker pull selenoid/vnc_chrome:89.0
+sudo docker pull selenoid/vnc_chrome:89.0
 echo "==============================> Pulling Firefox"
-docker pull selenoid/vnc_firefox:87.0
+sudo docker pull selenoid/vnc_firefox:87.0
 echo "Starting Selenoid Service ... "
-docker-compose up --force-recreate -d
+sudo docker-compose up --force-recreate -d
