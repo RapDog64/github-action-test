904f8f95bcce20c12918392943a480b1e213fc44
Denis <forest152324@gmail.com>
2021-12-08 22:51:50 +0400

    rollback

--- selenoid_start.sh
+++ selenoid_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "==============================> Pulling Chrome"
-sudo docker pull selenoid/vnc_chrome:89.0
+docker pull selenoid/vnc_chrome:89.0
 echo "==============================> Pulling Firefox"
-sudo docker pull selenoid/vnc_firefox:87.0
+docker pull selenoid/vnc_firefox:87.0
 echo "Starting Selenoid Service ... "
-sudo docker-compose up --force-recreate -d
+docker-compose up --force-recreate -d
