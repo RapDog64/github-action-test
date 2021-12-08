69b40315d4381e60204a72fddfc03e85e089147a
Denis <forest152324@gmail.com>
2021-12-08 22:00:30 +0400

    added remote selenoid

--- src/test/java/org/github/tests/LoginTest.java
+++ src/test/java/org/github/tests/LoginTest.java
@@ -5,13 +5,16 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
@@ -22,11 +25,24 @@ public class LoginTest {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "http://demowebshop.tricentis.com";
+
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+        Configuration.remote = "http://0.0.0.0:4444/wd/hub";
+
+        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
     void configureBrowser() {
         SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterEach
+    void closeTheDriver() {
+        closeWebDriver();
     }
 
     @Test
